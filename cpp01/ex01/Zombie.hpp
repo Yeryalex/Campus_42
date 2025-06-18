cfac206a33aef2a5cf96280cc9ac7ac09d94bd5c
Yery Rodriguez <128627025+Yeryalex@users.noreply.github.com>
2025-06-18 16:55:24 +0200

    Create multiple Zombies...

---
+++ cpp01/ex01/Zombie.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yrodrigu <yrodrigu@student.42barcelo>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/18 13:12:50 by yrodrigu          #+#    #+#             */
+/*   Updated: 2025/06/18 16:44:45 by yrodrigu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <iostream>
+#include <string>
+
+class Zombie
+{
+	private:
+		std::string name;
+	public:
+		Zombie(std::string name);
+		~Zombie(){};
+		void announce(void);
+};
+
+Zombie* zombieHorde( int N, std::string name );
+
+#endif
---
+++ cpp01/ex01/zombieHorde.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   zombieHorde.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yrodrigu <yrodrigu@student.42barcelo>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/18 16:41:38 by yrodrigu          #+#    #+#             */
+/*   Updated: 2025/06/18 16:53:55 by yrodrigu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie* zombieHorde( int N, std::string name )
+{
+	Zombie *zombieHorde;
+
+	zombieHorde = &(new Zombie(name))[N];
+	return (zombieHorde);
+}
