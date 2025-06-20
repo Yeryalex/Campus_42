06bd18039a27da9461bc7fec0fbb25ac52032c92
Yery Rodriguez <128627025+Yeryalex@users.noreply.github.com>
2025-06-20 14:32:00 +0200

    Connect functions to the program so that interchange strings in a file

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yrodrigu <yrodrigu@student.42barcelo>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 15:49:22 by yrodrigu          #+#    #+#             */
-/*   Updated: 2025/06/20 13:03:36 by yrodrigu         ###   ########.fr       */
+/*   Updated: 2025/06/20 14:28:18 by yrodrigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,34 @@
 #include <string>
 #include <fstream>
 
-void	ft_replace(std::string line, std::string s1, std::string s2)
+std::string	ft_replace(std::string line, std::string s1, std::string s2)
 {
 	std::string	newLine;
 	int			found = line.find(s1);
 	int			initialPos = 0;
 
-	while (found && found != -1)
+	if (s1.empty())
+		return (newLine = line);
+	while (found != -1)
 	{
 		newLine = newLine.append(line.substr(initialPos, found).append(s2));
 		line = line.substr(found + s1.length());
 		found = line.find(s1);
 	}
 	newLine.append(line);
-	std::cout << newLine;
+	return (newLine);
 }
-/*
-int	main()
+
+std::string	fileExtension(std::string filename)
 {
-	std::string	line;
-	std::string	s1;
-	std::string s2;
+	int	existFileExt = filename.find(".");
 
-	line = "Hola mundo, del uno al dos hay uno de diferencia\n";
-	s1 = " ";
-	s2 = "|";
-
-	ft_replace(line, s1, s2);
-	return (0);
+	if (existFileExt != -1)
+		filename = filename.substr(0, existFileExt);
+	
+	filename.append(".replace");
+	return (filename);
 }
-*/
 
 int	main(int argc, char **argv)
 {
@@ -52,18 +50,19 @@ int	main(int argc, char **argv)
 		std::cout << "Please, enter valid arguments: <filename> String1 string2\n";
 		return (0);
 	}
+	
 	std::string	filenameNew = argv[1];
+	std::string s1 = argv[2];
+	std::string s2 = argv[3];
 
-	filenameNew.append(".replace");
+	filenameNew = fileExtension(filenameNew);
 	std::ifstream	readFile(argv[1]);
 	std::ofstream	newFile(filenameNew.c_str());
-
 	std::string line;
+	
 	while (getline(readFile, line))
-	{
-		newFile << line;
-		std::cout << line << std::endl;
-	}
+		newFile << ft_replace(line, s1, s2) << std::endl;
+	
 	newFile.close();
 	readFile.close();
 	return (0);
