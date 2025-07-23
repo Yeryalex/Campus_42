597cc305c5dc92a3f113449c3d7d0e5d07e66f87
Yery Rodriguez <128627025+Yeryalex@users.noreply.github.com>
2025-07-23 21:36:56 +0200

    Add Exceptions to the Buraucrat class

---
+++ cpp05/ex00/Bureaucrat.cpp
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yrodrigu <yrodrigu@student.42barcelo>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/22 14:46:15 by yrodrigu          #+#    #+#             */
+/*   Updated: 2025/07/23 21:24:46 by yrodrigu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+const char* Bureaucrat::GradeTooHighException::what() const throw() {
+	
+	return ("Grade provided is too HIGH! (range 1 - 150).\n");
+};
+
+const char *Bureaucrat::GradeTooLowException::what() const throw() {
+	return ("Grade provided is too LOW! (range 1 - 150).\n");
+};
+
+Bureaucrat::Bureaucrat(): name("default_bureaucrat"), grade(150) {}
+
+Bureaucrat::Bureaucrat(const std::string &name, int grade): name(name) {
+	if (grade > 150)
+		throw GradeTooLowException();
+	if (grade < 1)
+		throw GradeTooHighException();
+	this->grade = grade;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &obj): name(obj.name), grade(obj.grade) {}
+
+Bureaucrat	&Bureaucrat::operator=(const Bureaucrat &obj)
+{
+	if (this != &obj)
+		grade = obj.grade;
+	return (*this);
+}
+
+Bureaucrat::~Bureaucrat(){}
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &obj)
+{
+	out << obj.getName() << ", bureaucrat grade " << obj.getGrade() << ".\n";
+	return (out);
+}
+
+Bureaucrat	&Bureaucrat::operator++()
+{
+	if (grade == 1)
+		throw GradeTooHighException();
+	this->grade--;
+	return (*this);
+}
+
+Bureaucrat	Bureaucrat::operator++(int)
+{
+	Bureaucrat	temp(*this);
+	++(*this);
+	return (temp);
+}
+
+Bureaucrat	&Bureaucrat::operator--()
+{
+	if (grade == 150)
+		throw GradeTooLowException();
+	this->grade++;
+	return (*this);
+}
+
+Bureaucrat Bureaucrat::operator--(int) {
+	
+	Bureaucrat temp(*this);
+	--(*this);
+	return (temp);
+}
+
+const std::string &Bureaucrat::getName(void) const
+{
+	return (this->name);
+}
+
+int	Bureaucrat::getGrade(void) const
+{
+	return (this->grade);
+}
+
+void	Bureaucrat::incrementGrade(void) {
+	
+	++(*this);
+}
+
+void	Bureaucrat::decrementGrade(void) {
+	
+	--(*this);
+}
---
+++ cpp05/ex00/main.cpp
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yrodrigu <yrodrigu@student.42barcelo>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/22 14:46:27 by yrodrigu          #+#    #+#             */
+/*   Updated: 2025/07/23 21:36:08 by yrodrigu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+int main() {
+
+	Bureaucrat Bob("Bob", 1);
+	std::cout << Bob;
+
+	try {
+		Bureaucrat a("Yery", 1);
+		a.incrementGrade();
+	}
+	catch (const std::exception &e) {
+		std::cout << e.what();
+	}
+	try {
+		Bureaucrat a("Yery", 150);
+		a.decrementGrade();
+	}
+	catch (const std::exception &e) {
+		std::cout << e.what();
+	}
+	try {
+		Bureaucrat a("Yery", 1);
+		a++;
+	}
+	catch (const std::exception &e) {
+		std::cout << e.what();
+	}
+	try {
+		Bureaucrat a("Yery", 150);
+		a--;
+	}
+	catch (const std::exception &e) {
+		std::cout << e.what();
+	}
+	try {
+		Bureaucrat a("Yery", 3);
+		a.incrementGrade();
+		std::cout << a;
+	}
+	catch (const std::exception &e) {
+		std::cout << e.what();
+	}
+	try {
+		Bureaucrat a("Yery", 149);
+		a.decrementGrade();
+		std::cout << a;
+	}
+	catch (const std::exception &e) {
+		std::cout << e.what();
+	}
+	try {
+		Bureaucrat a("Yery", 3);
+		a.incrementGrade();
+		a++;
+		std::cout << a;
+	}
+	catch (const std::exception &e) {
+		std::cout << e.what();
+	}
+	try {
+		Bureaucrat a("Yery", 3);
+		a.incrementGrade();
+		a++;
+		++a;
+		std::cout << a;
+	}
+	catch (const std::exception &e) {
+		std::cout << e.what();
+	}
+	return (0);
+}
