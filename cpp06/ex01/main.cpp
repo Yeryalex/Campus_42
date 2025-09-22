8a5aa55053951f8251dd4cfc4702ef17b74d0a77
Yery Rodriguez <128627025+Yeryalex@users.noreply.github.com>
2025-09-22 12:51:06 +0200

    Implement the functions required in the main program

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yrodrigu <yrodrigu@student.42barcelo>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/19 11:20:57 by yrodrigu          #+#    #+#             */
-/*   Updated: 2025/09/22 09:56:21 by yrodrigu         ###   ########.fr       */
+/*   Updated: 2025/09/22 12:36:34 by yrodrigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,18 @@
 int main() {
 
 	Data filledData = {28, "Yery"};
-//////////////////////////////////////////////////////////////////
+	
 	uintptr_t	raw = Serializer::serialize(&filledData);
 
 	std::cout << "--- uint Value and Memory address ---\n" << std::endl;
 	std::cout << "unsigned int:     ";
 	std::cout << raw << std::endl;
-/////////////////////////////////////////////////////////////////
+	
 	Data	*restored = Serializer::deserialize(raw);
 	
 	std::cout << "Data serialize:   " << &filledData << std::endl;	
 	std::cout << "Data deserialize: " << restored << std::endl;
 
-/////////////////////////////////////////////////////////////////
 	std::cout << std::endl << "--- Values after deserialize ---\n" << std::endl;
 	std::cout << "X:    " << restored->x << std::endl;
 	std::cout << "Name: " << restored->name << std::endl;
--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -6,11 +6,59 @@
 /*   By: yrodrigu <yrodrigu@student.42barcelo>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 10:38:33 by yrodrigu          #+#    #+#             */
-/*   Updated: 2025/09/22 12:32:07 by yrodrigu         ###   ########.fr       */
+/*   Updated: 2025/09/22 12:47:59 by yrodrigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "identifyClass.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+Base    *generate(void) {
+
+    Base    *arrBase[] = {new A(), new B(), new C()};
+
+    srand(time(0));
+    int num = rand() % 3;
+
+    return (arrBase[num]);
+}
+
+void    identify(Base *p) {
+
+    if (dynamic_cast<A*>(p))
+        std::cout << "A" << std::endl;
+    else if (dynamic_cast<B*>(p))
+        std::cout << "B" << std::endl;
+    else if (dynamic_cast<C*>(p))
+        std::cout << "C" << std::endl;
+    else
+        std::cout << "Unknown" << std::endl;
+}
+
+void    identify(Base &p) {
+
+    try {
+        (void)dynamic_cast<A&>(p);
+        std::cout << "A" << std::endl;
+        return ;
+    }
+    catch (std::exception &e) { }
+
+    try {
+        (void)dynamic_cast<B&>(p);
+        std::cout << "B" << std::endl;
+        return ;
+    }
+    catch (std::exception &e) { }
+
+    try {
+        (void)dynamic_cast<C&>(p);
+        std::cout << "C" << std::endl;
+        return ;
+    }
+    catch (std::exception &e) { }
+}
 
 int	main() {
 	
