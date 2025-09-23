cd0eefe6bb5febc1e0004a7d05527e35a3bdc71e
Yery Rodriguez <128627025+Yeryalex@users.noreply.github.com>
2025-09-23 12:40:53 +0200

    Make ne folder and set the bases for array exercise

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -1,40 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   iter.hpp                                           :+:      :+:    :+:   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yrodrigu <yrodrigu@student.42barcelo>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/23 10:10:30 by yrodrigu          #+#    #+#             */
-/*   Updated: 2025/09/23 11:52:57 by yrodrigu         ###   ########.fr       */
+/*   Created: 2025/09/23 12:38:54 by yrodrigu          #+#    #+#             */
+/*   Updated: 2025/09/23 12:40:19 by yrodrigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ITER_HPP
-# define	ITER_HPP
+#ifndef ARRAY_HPP
+# define	ARRAY_HPP
 
 #include <iostream>
-
-void	printerInt(const int value){
-
-	std::cout << value << " ";
-}
-
-template	<typename T>
-void	print(const T &value) {
-	std::cout << value << " ";
-}
-
-template	<typename T>
-void	increment(T &a) {
-	a++;
-}
-
-template	<typename T, typename function>
-void	iter(T arr[], size_t arr_len, function f) { 
-	for (size_t i = 0; i < arr_len; i++) {
-		f(arr[i]);
-	}
-}
 
 #endif
